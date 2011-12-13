331f4d2bf719c9f50dda6037675c9210b22400c2
Tony Finn <tony@tonyfinn.com>
2011-12-13 13:41:27 +0000

    Allow the ability to specify a config file on the command line.
    --HG--
    branch : multi-server

--- pyircd.py
+++ pyircd.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 import asyncore
+import argparse
 import logging
+import sys
 
 from pyircd.config import Config
 from pyircd.network import IRCNetwork
@@ -11,7 +13,15 @@ VERSION="dev-2"
 class PyIRCD:
     """Controls the running and setup of the IRC Server"""
     def __init__(self):
-        self.config = Config()
+        parser = argparse.ArgumentParser(description='Run the IRC Server.')
+        parser.add_argument('--config', '-c', nargs='?', help='Path to the'
+        ' config file', default='config.json')
+        args = parser.parse_args()
+        try:
+            self.config = Config(args.config)
+        except IOError:
+            print('Invalid config file specified.')
+            sys.exit(1)
         self.config.version = VERSION
         logging.basicConfig(level=logging.DEBUG)
         self.network = IRCNetwork(self.config)
